62ba6aadcb6c3840f875a4b2e7c8826b853e4b33
NickArtCorp <ncorp237@gmail.com>
2024-02-12 14:44:52 +0100

    New commit

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+function Nav() {
+    return (
+      <>
+       <nav>
+
+        <ul>
+         <li>Home</li>
+         <li>Services</li>
+         <li>About</li>
+         <li>Me</li>
+        </ul>
+
+       </nav>
+      </>
+    );
+  }
+
+  export default Nav;
